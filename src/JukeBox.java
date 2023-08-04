3a369c7a6260c884a89a897aea6537610123bb9b
Brandon Stamba <brandonstamba@gmail.com>
2023-08-04 14:47:23 -0500

    Generics & Sorting Version 3

--- src/JukeBox.java
+++ src/JukeBox.java
@@ -6,12 +6,18 @@ public class JukeBox {
         List<Songs> songList = SongInfo.getSongStrings();
         //Original list.
         System.out.println(songList);
+
         //List sorted by title.
         Collections.sort(songList);
         System.out.println(songList);
+
         //List sorted by artist.
         ArtistCompare aCompare = new ArtistCompare();
         songList.sort(aCompare);
+        System.out.println(songList);
+
+        //List sorted with lambda.
+        songList.sort((one, two) -> one.getArtist().compareTo(two.getArtist()));
         System.out.println(songList);
     }
 }
